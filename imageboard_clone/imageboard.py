fb209f0303d527282d6921894cafb32c4d30da47
samirboudissa1994 <samirboudissa1@msn.com>
2019-06-13 22:51:04 +0300

    edits

--- imageboard_clone/imageboard.py
+++ imageboard_clone/imageboard.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def imageboard():
-    return "This will be an imageboard"
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
